d89859dcbf1bb1509a2d1d780e715ab69e918dee
abdul ghani <mohammedghani842@gmail.com>
2021-09-25 03:11:46 -0700

    applied changes' ' '

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ class App extends React.Component {
     super();
     this.state = {
       albums: [
-        {id: 1, artist: "Vijay T", album_title: "Master", album_cover: "Music Anirudh R", songs: "Sound Track1",img:"https://th.bing.com/th/id/OIP.PG890d22ZaRLFfurF5o4rwAAAA?w=125&h=180&c=7&o=5&dpr=1.5&pid=1.7"},
-        {id: 2, artist: "Naveen Polishetty", album_title: "Jathirathnalu", album_cover: "Music Radhan", songs: "Sound Track2",img:"https://th.bing.com/th/id/OIP.UfqMH8JseiJlHF9HpsjVywHaHI?w=217&h=209&c=7&o=5&dpr=1.5&pid=1.7"},
-        {id: 3, artist: "Suriya", album_title: "Aakasam Nee Haddura", album_cover: "Music G. V. Prakash", songs: "Sound Track3",img:"https://th.bing.com/th/id/OIP.dqgZ9Xht3MKl8yHDrUpCbgHaGU?w=208&h=180&c=7&o=5&dpr=1.5&pid=1.7"},
+        {id: 1, artist: "Ariit singh", album_title: "Tum hi ho", album_cover: "Arijit singh", songs: "Sound Track1",img:"https://www.india.com/wp-content/uploads/2013/12/tum-hi-ho-aashiqui2.jpg"},
+        {id: 2, artist: "Atif aslam", album_title: "Kun Faya Kun", album_cover: "Atif Aslam", songs: "Qawwali",img:"https://i.ytimg.com/vi/XuKmrzrUxYw/hqdefault.jpg"},
+        {id: 3, artist: "jayshree", album_title: "Zara Zara", album_cover: "Music Dhavan", songs: "Sound Track3",img:"https://i.ytimg.com/vi/0Jj9rOwkfLM/hqdefault.jpg"},
+        
       ],
       errorMsg: ''
     }
--- src/Components/Album.js
+++ src/Components/Album.js
@@ -10,14 +10,8 @@ export class Album extends Component {
     render() {
         return (
             <div className = "card">
-                <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="https://th.bing.com/th/id/OIP.O5tUNJsGg-30Evxw7tIzDQHaKl?w=136&h=194&c=7&o=5&dpr=1.5&pid=1.7" width="300" height="380" />}
-                >
-                    <Meta title= {this.props.album_title}/>
-                    <h5>{this.props.artist}</h5>
-                    <h5>{this.props.album_cover}</h5>
+                <Card>
+                
                 </Card>
             </div>
         )
